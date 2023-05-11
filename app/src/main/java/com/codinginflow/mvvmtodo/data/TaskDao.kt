07ef054290cdf0f6ece89c9ca0be8bc1c89f5e53
Brian <developing.brian@gmail.com>
2023-05-10 20:10:50 -0700

    Combine multiple flows in search query - section 7

--- app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
@@ -1,17 +1,28 @@
 package com.codinginflow.mvvmtodo.data
 
 import androidx.room.*
+import com.codinginflow.mvvmtodo.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
+    // Can call this function from the outside passing in SortOrder to use on below queries
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder) {
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
+
     // Flow represents a stream of data; whenever a change in database table,
     // Room puts a new list of tasks into the Flow
     // Flow can only be used or collected inside a coroutine; this is why no suspend keyword used;
     // all suspension happens inside the Flow; Flow is an asynchronous stream of data
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
 // TODO: @ViewModelInject is deprecated; Now use @HiltViewModel
@@ -14,8 +15,21 @@ class TasksViewModel @ViewModelInject constructor(
 
     val searchQuery = MutableStateFlow("")
 
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+
+    // Use `combine`, which is part of Flow library, to combine multiple Flows.
+    // If any 1 value changes, get back latest values of all 3 values
+    private val tasksFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) {
+        query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)  // Kotlin can only return a single value, so use Triple to wrap them up
+    }
+        .flatMapLatest { (query, sortOrder, hideCompleted) ->   // Destructuring here for ease of use
+        taskDao.getTasks(query, sortOrder, hideCompleted)
     }
 
     // Common practice to use Flow below the ViewModel, then transform to LiveData inside ViewModel so we can observe the LiveData in the Fragment.
@@ -23,4 +37,7 @@ class TasksViewModel @ViewModelInject constructor(
     // Flow is more flexible; has different operators to transform data; can switch thread in the Flow; don't lose data since it's a stream.
     // LiveData has a benefit that makes handling the lifecycle of fragment easier because it is lifecycle aware (eg: if fragment goes in background and becomes inactive, LiveData would detect and stop dispatching events)
     val tasks = tasksFlow.asLiveData()
+
 }
+
+enum class SortOrder { BY_NAME, BY_DATE }
